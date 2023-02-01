675440387e21ef7a57b1f4b56d8bf35c4d93667e
artehe <112902041+artehe@users.noreply.github.com>
2023-02-01 19:00:30 +0000

    Add missing plist exception

---
+++ Netimobiledevice/Exceptions/PlistException.cs
@@ -0,0 +1,10 @@
+﻿namespace Netimobiledevice.Exceptions;
+
+internal class PlistException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlistException"/> class.
+    /// </summary>
+    /// <param name="message">Message.</param>
+    public PlistException(string message) : base(message) { }
+}
