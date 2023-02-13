88d6e0ca362aa6fedaa495b3d19ba5f612294f3b
artehe <112902041+artehe@users.noreply.github.com>
2023-02-13 08:36:06 -0800

    Add UsbmuxdLookupOptions flags

---
+++ Netimobiledevice/Usbmuxd/UsbmuxdLookupOptions.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Netimobiledevice.Usbmuxd
+{
+    /// <summary>
+    /// Device lookup options for usbmuxd_get_device.
+    /// </summary>
+    [Flags]
+    public enum UsbmuxdLookupOptions : int
+    {
+        /// <summary>
+        /// Include USB connected devices during lookup.
+        /// </summary>
+        Usb = 1 << 1,
+        /// <summary>
+        /// Include network connected devices during lookup.
+        /// </summary>
+        Network = 1 << 2,
+        /// <summary>
+        /// Prefer network connection.
+        /// </summary>
+        PreferNetwork = 1 << 3
+    }
+}
