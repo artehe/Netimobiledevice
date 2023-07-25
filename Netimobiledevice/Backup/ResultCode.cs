6f15e24b6a3dac0210f49bad0a9a94febdd922ea
artehe <112902041+artehe@users.noreply.github.com>
2023-07-25 09:34:02 +0100

    Add missing result code namespace

---
+++ Netimobiledevice/Backup/ResultCode.cs
@@ -0,0 +1,14 @@
+﻿namespace Netimobiledevice.Backup
+{
+    /// <summary>
+    /// Result codes for Mobilebackup2 Service operations.
+    /// </summary>
+    public enum ResultCode : byte
+    {
+        Success = 0x00,
+        LocalError = 0x06,
+        RemoteError = 0x0B,
+        FileData = 0x0C,
+        Skipped = 0xFF
+    }
+}
