f88d0491844edc150bdf1f2df388d9ced25cb8ad
Elanchezhiyan P <elanche97@hotmail.com>
2023-12-16 12:00:13 +0530

    Added the iPhone model identifiers
    #13 Added the iPhone model identifiers to identify the iPhone model such as iPhone 7, iPhone 8 or iPhone 15 pro max.

--- NetimobiledeviceDemo/Program.cs
+++ NetimobiledeviceDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Netimobiledevice.Afc;
 using Netimobiledevice.Backup;
+using Netimobiledevice.HelperFiles;
 using Netimobiledevice.Lockdown;
 using Netimobiledevice.Lockdown.Services;
 using Netimobiledevice.Misagent;
@@ -37,6 +38,10 @@ public class Program
         }
 
         await Task.Delay(1000);
+
+        using (LockdownClient lockdown = LockdownClient.CreateLockdownClient(testDevice?.Serial ?? string.Empty)) {
+            Console.WriteLine($"Current connected model : {ModelIdentifier.GetDeviceModelName(lockdown.Product)}");
+        }
 
         using (LockdownClient lockdown = LockdownClient.CreateLockdownClient(testDevice?.Serial ?? string.Empty)) {
             string product = lockdown.Product;
