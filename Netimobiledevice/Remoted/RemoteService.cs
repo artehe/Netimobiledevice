8ff36aba15b2c0f3ced1c40fa5fbb7d5040271ad
artehe <112902041+artehe@users.noreply.github.com>
2024-10-11 16:32:20 +0100

    first remote connection files

---
+++ Netimobiledevice/Remoted/RemoteService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Netimobiledevice.Remoted
+{
+    internal class RemoteService : IAsyncDisposable
+    {
+        private string _serviceName;
+        private RemoteServiceDiscoveryService _rsd;
+        private RemoteXPCConnection? _service;
+
+        public RemoteService(RemoteServiceDiscoveryService rsd, string serviceName)
+        {
+            _serviceName = serviceName;
+            _rsd = rsd;
+        }
+
+        public async Task Connect()
+        {
+            _service = _rsd.StartRemoteService(_serviceName);
+            await _service.Connect().ConfigureAwait(false);
+        }
+
+        public async Task Close()
+        {
+            await _service?.Close().ConfigureAwait(false);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Close().ConfigureAwait(false);
+        }
+    }
+}
