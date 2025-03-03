068854545808b0fa585515aa21b11869448bd501
artehe <112902041+artehe@users.noreply.github.com>
2025-03-03 10:59:18 +0000

    add async SendCommand function

--- Netimobiledevice/Afc/HouseArrestService.cs
+++ Netimobiledevice/Afc/HouseArrestService.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Logging;
+using Netimobiledevice.Exceptions;
 using Netimobiledevice.Lockdown;
 using Netimobiledevice.Plist;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Netimobiledevice.Afc
 {
@@ -11,15 +14,12 @@ namespace Netimobiledevice.Afc
         private const string VEND_CONTAINER = "VendContainer";
         private const string VEND_DOCUMENTS = "VendDocuments";
 
-        private readonly bool documentsOnly;
-
         public HouseArrestService(LockdownServiceProvider lockdown, string serviceName, string bundleId, bool documentsOnly = false, ILogger? logger = null) : base(lockdown, serviceName, logger)
         {
             string cmd = VEND_CONTAINER;
             if (documentsOnly) {
                 cmd = VEND_DOCUMENTS;
             }
-            this.documentsOnly = documentsOnly;
 
             try {
                 this.SendCommand(bundleId, cmd);
@@ -34,17 +34,48 @@ namespace Netimobiledevice.Afc
 
         public HouseArrestService(LockdownClient lockdown, string bundleId, ILogger? logger = null) : this(lockdown, SERVICE_NAME, bundleId, false, logger) { }
 
-        public void SendCommand(string bundleId, string cmd = VEND_CONTAINER)
+        public DictionaryNode SendCommand(string bundleId, string cmd = VEND_CONTAINER)
         {
             DictionaryNode request = new DictionaryNode() {
                 { "Command", new StringNode(cmd) },
                 { "Identifier", new StringNode(bundleId) }
             };
+
             PropertyNode? response = this.Service.SendReceivePlist(request);
+
             DictionaryNode responseDict = response?.AsDictionaryNode() ?? [];
             if (responseDict.TryGetValue("Error", out PropertyNode? value)) {
-                throw new AfcException(value.AsStringNode().Value);
+                string err = value.AsStringNode().Value;
+                if (err == "ApplicationLookupFailed") {
+                    throw new AfcException($"No app with bundle id {bundleId} found");
+                }
+                else {
+                    throw new NetimobiledeviceException(err);
+                }
             }
+            return responseDict;
+        }
+
+        public async Task<DictionaryNode> SendCommandAsync(string bundleId, string cmd = VEND_CONTAINER, CancellationToken cancellationToken = default)
+        {
+            DictionaryNode request = new DictionaryNode() {
+                { "Command", new StringNode(cmd) },
+                { "Identifier", new StringNode(bundleId) }
+            };
+
+            PropertyNode? response = await this.Service.SendReceivePlistAsync(request, cancellationToken).ConfigureAwait(false);
+
+            DictionaryNode responseDict = response?.AsDictionaryNode() ?? [];
+            if (responseDict.TryGetValue("Error", out PropertyNode? value)) {
+                string err = value.AsStringNode().Value;
+                if (err == "ApplicationLookupFailed") {
+                    throw new AfcException($"No app with bundle id {bundleId} found");
+                }
+                else {
+                    throw new NetimobiledeviceException(err);
+                }
+            }
+            return responseDict;
         }
     }
 }
