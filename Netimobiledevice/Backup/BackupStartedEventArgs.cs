fe1d71b725064806ee7462298aa026a7e62ec478
artehe <112902041+artehe@users.noreply.github.com>
2025-06-04 10:49:45 +0100

    trigger started event when we should now.

---
+++ Netimobiledevice/Backup/BackupStartedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Netimobiledevice.Backup
+{
+    /// <summary>
+    /// EventArgs for backup started events.
+    /// </summary>
+    public class BackupStartedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The iOS version for the backup
+        /// </summary>
+        public Version IosVersion { get; }
+
+        /// <summary>
+        /// Creates an instance of the BackupStartedEventArgs class.
+        /// </summary>
+        /// <param name="iosVersion">The iOS version for the device.</param>
+        public BackupStartedEventArgs(Version iosVersion)
+        {
+            IosVersion = iosVersion;
+        }
+    }
+}
