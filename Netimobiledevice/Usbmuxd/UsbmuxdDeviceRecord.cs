c5191d4bdaff55b343ed14ffabc60d9b732ad4a4
artehe <112902041+artehe@users.noreply.github.com>
2025-09-04 14:40:34 +0100

    Implement specific from to to byte methods.

--- Netimobiledevice/Usbmuxd/UsbmuxdDeviceRecord.cs
+++ Netimobiledevice/Usbmuxd/UsbmuxdDeviceRecord.cs
@@ -1,13 +1,25 @@
-﻿using System.Runtime.InteropServices;
+﻿using Netimobiledevice.EndianBitConversion;
+using System.Text;
 
 namespace Netimobiledevice.Usbmuxd;
 
-[StructLayout(LayoutKind.Sequential, Pack = 1)]
 internal struct UsbmuxdDeviceRecord
 {
     public uint DeviceId;
     public short ProductId;
-    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
     public string SerialNumber;
     public int Location;
+
+    public static UsbmuxdDeviceRecord FromBytes(byte[] bytes)
+    {
+        int stringLength = EndianBitConverter.LittleEndian.ToInt32(bytes, 6);
+        string serial = Encoding.UTF8.GetString(bytes, 10, stringLength);
+
+        return new UsbmuxdDeviceRecord() {
+            DeviceId = EndianBitConverter.LittleEndian.ToUInt32(bytes, 0),
+            ProductId = EndianBitConverter.LittleEndian.ToInt16(bytes, 4),
+            SerialNumber = Encoding.UTF8.GetString(bytes, 10, stringLength),
+            Location = EndianBitConverter.LittleEndian.ToInt32(bytes, 10 + stringLength)
+        };
+    }
 }
