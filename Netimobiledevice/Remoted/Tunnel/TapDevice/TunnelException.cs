65aeeb6db8a7fdd0b8e14957b6a34f927a2310f5
artehe <112902041+artehe@users.noreply.github.com>
2026-05-11 14:40:49 +0100

    Hanlde tuntapdevice os support

---
+++ Netimobiledevice/Remoted/Tunnel/TapDevice/TunnelException.cs
@@ -0,0 +1,7 @@
+﻿namespace Netimobiledevice.Remoted.Tunnel.TapDevice;
+
+public class TunnelException : NetimobiledeviceException {
+    public TunnelException() : base() { }
+
+    public TunnelException(string message) : base(message) { }
+}
