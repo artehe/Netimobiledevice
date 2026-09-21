ecbd1801d45a19ad3e356b4dfd927b3e3c123cc4
artehe <112902041+artehe@users.noreply.github.com>
2026-09-21 14:46:39 +0100

    Fix data node issues.

--- NetimobiledeviceTest/Plist/DataNodeTests.cs
+++ NetimobiledeviceTest/Plist/DataNodeTests.cs
@@ -3,23 +3,20 @@
 namespace NetimobiledeviceTest.Plist;
 
 [TestClass]
-public class DataNodeTests
-{
+public class DataNodeTests {
     [TestMethod]
-    public void ByteSequenceIsCorrectUsingShortArray()
-    {
+    public void ByteSequenceIsCorrectUsingShortArray() {
         byte[] initialByteArray = [255];
 
         DataNode node = new DataNode(initialByteArray);
-        Assert.AreEqual(1, node.Value.Length);
+        Assert.HasCount(1, node.Value);
 
         byte[] returntedByteArray = node.Value;
-        Assert.AreEqual(initialByteArray, returntedByteArray);
+        Assert.AreSequenceEqual(initialByteArray, returntedByteArray);
     }
 
     [TestMethod]
-    public void ByteSequenceIsCorrectUsingLongArray()
-    {
+    public void ByteSequenceIsCorrectUsingLongArray() {
         List<byte> longByteArray = [];
         for (int i = 1; i < 256; i++) {
             longByteArray.Add((byte) i);
@@ -27,62 +24,57 @@ public class DataNodeTests
         byte[] initialByteArray = [.. longByteArray];
 
         DataNode node = new DataNode(initialByteArray);
-        Assert.AreEqual(255, node.Value.Length);
+        Assert.HasCount(255, node.Value);
 
         byte[] returntedByteArray = node.Value;
-        Assert.AreEqual(initialByteArray, returntedByteArray);
+        Assert.AreSequenceEqual(initialByteArray, returntedByteArray);
     }
 
     [TestMethod]
-    public void ByteSequenceIsCorrectUsingZeroByteValueAtStart()
-    {
+    public void ByteSequenceIsCorrectUsingZeroByteValueAtStart() {
         byte[] initialByteArray = [0, 1, 2, 3, 4, 5, 6, 7];
 
         DataNode node = new DataNode(initialByteArray);
-        Assert.AreEqual(8, node.Value.Length);
+        Assert.HasCount(8, node.Value);
 
         byte[] returntedByteArray = node.Value;
-        Assert.AreEqual(initialByteArray, returntedByteArray);
+        Assert.AreSequenceEqual(initialByteArray, returntedByteArray);
     }
 
     [TestMethod]
-    public void ByteSequenceIsCorrectUsingZeroByteValueInMiddle()
-    {
+    public void ByteSequenceIsCorrectUsingZeroByteValueInMiddle() {
         byte[] initialByteArray = [1, 2, 3, 4, 0, 3, 2, 1];
 
         DataNode node = new DataNode(initialByteArray);
-        Assert.AreEqual(8, node.Value.Length);
+        Assert.HasCount(8, node.Value);
 
         byte[] returntedByteArray = node.Value;
-        Assert.AreEqual(initialByteArray, returntedByteArray);
+        Assert.AreSequenceEqual(initialByteArray, returntedByteArray);
     }
 
     [TestMethod]
-    public void ByteSequenceIsCorrectUsingZeroByteValueAtEnd()
-    {
+    public void ByteSequenceIsCorrectUsingZeroByteValueAtEnd() {
         byte[] initialByteArray = [1, 2, 3, 4, 5, 6, 7, 0];
 
         DataNode node = new DataNode(initialByteArray);
-        Assert.AreEqual(8, node.Value.Length);
+        Assert.HasCount(8, node.Value);
 
         byte[] returntedByteArray = node.Value;
-        Assert.AreEqual(initialByteArray, returntedByteArray);
+        Assert.AreSequenceEqual(initialByteArray, returntedByteArray);
     }
 
     [TestMethod]
-    public void ToStringReturnsNodeType()
-    {
+    public void ToStringReturnsNodeType() {
         DataNode value = new DataNode([0, 1, 2, 3, 4, 5, 6, 7]);
         Assert.AreEqual($"<data>: System.Byte[]", value.ToString());
     }
 
     [TestMethod]
-    public void PropertyNodeConvertsToByteArray()
-    {
+    public void PropertyNodeConvertsToByteArray() {
         byte[] initialByteArray = [0, 1, 2, 3, 4, 5, 6, 7];
         PropertyNode property = new DataNode(initialByteArray);
         byte[] returntedByteArray = ((DataNode) property).Value;
-        Assert.AreEqual(initialByteArray, returntedByteArray);
+        Assert.AreSequenceEqual(initialByteArray, returntedByteArray);
     }
 
 }
